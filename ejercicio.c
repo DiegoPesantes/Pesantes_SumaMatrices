e8f5175b340aa515b3bec3a5495a49736116ae51
Laboratorio <laboratorio@udla-ec.int>
2023-11-28 15:23:49 -0500

    Generamos la primera matriz

--- ejercicio.c
+++ ejercicio.c
@@ -11,7 +11,28 @@
     int PM[F][C];
     int SM[F][C];
     int RES[F][C];
-    
+    for (int i = 0; i < F; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            PM[i][j]= rand()%101;
+            SM[i][j]= rand()%101;
+            RES[i][j] = PM[i][j]+SM[i][j];
+        }
+        //Imprimimos primera matriz//
+    }
+        printf("Primera Matriz:\n");
+
+        for (int i = 0; i < F; i++)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                printf("%d", PM[i][j]);
+            }
+            printf(" \n");
+        }
+        //Imprimimos segunda matriz//
+        
     return 0;
  }
  
