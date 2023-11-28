389a2e1db64c0be8eca15b3a7ab422e8675fb98e
Laboratorio <laboratorio@udla-ec.int>
2023-11-28 15:21:21 -0500

    Creacion de variables para la suma

---
+++ ejercicio.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main(int argc, char const *argv[])
+ {
+    int F, C;
+    srand(time(NULL));
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &F);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &C);
+    int PM[F][C];
+    int SM[F][C];
+    int RES[F][C];
+    for (int i = 0; i < F; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            PM[i][j]= rand()%101;
+            SM[i][j]= rand()%101;
+            RES[i][j] = PM[i][j]+SM[i][j];
+        }
+        //Imprimimos primera matriz//
+    }
+        printf("Primera Matriz:\n");
+
+        for (int i = 0; i < F; i++)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                printf("%d", PM[i][j]);
+            }
+            printf(" \n");
+        }
+        //Imprimimos segunda matriz//
+        printf("Segunda Matriz: \n");
+
+        for (int i = 0; i < F; i++)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                printf("%d", SM[i][j]);
+            }
+            printf(" \n");
+        }
+
+        //Sumamos la matrices
+         printf("RESULTADO:\n");
+        for (int i = 0; i < F; i++)
+        {
+            for (int j = 0; j < C; j++)
+            {
+                printf("%d", RES[i][j]);
+            }
+            printf(" \n");
+        }
+    return 0;
+ }
+ 
